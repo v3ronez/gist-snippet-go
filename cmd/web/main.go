c5f1d6d8d0217a5b446670b93942c71bc262853e
Henrique Veronez <v3ronez.dev@gmail.com>
2025-01-17 23:37:59 -0300

    flag parse and slog

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -3,24 +3,35 @@ package main
 import (
 	"flag"
 	"log"
+	"log/slog"
 	"net/http"
 )
 
+type config struct {
+	addr      string
+	staticDir string
+}
+
 func main() {
+	var cfg config
+
+	flag.StringVar(&cfg.addr, "addr", ":4000", "Http Network address")
+	flag.StringVar(&cfg.staticDir, "static-dir", "./ui/static", "Path to static assets")
+
+	flag.Parse()
 
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
 
-	fileStaticServer := http.FileServer(http.Dir("./ui/static"))
+	fileStaticServer := http.FileServer(http.Dir(cfg.staticDir))
 	mux.Handle("/static/", http.StripPrefix("/static", fileStaticServer))
-	addr := flag.String("addr", ":4000", "Http Network address")
-	flag.Parse()
 
-	log.Printf("Server running on %s", *addr)
+	log.Printf("Server running on %s\n", cfg.addr)
+	slog.Info("Server configs:", "addr", cfg.addr, "static-dir", cfg.staticDir)
 
-	err := http.ListenAndServe(*addr, mux)
+	err := http.ListenAndServe(cfg.addr, mux)
 	if err != nil {
 		log.Fatal(err)
 	}
